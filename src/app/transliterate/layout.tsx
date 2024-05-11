a384c8908d83e92c529dcec97fbc7f47fd8b8d8a
Alvyn Abranches <alvynabranches@gmail.com>
2024-05-11 11:10:50 +0530

    Updated Metadata

--- src/app/transliterate/layout.tsx
+++ src/app/transliterate/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Konkani Speech Dataset Review Page",
+    description: "Here the users will review the Konkani sentences in Roman and Devanagari scripts and its translation in English done by the other users.",
+};
+
 export default function TransliterateLayout() {
     return (<p>
         <h1>Transliterate from Devanagari to Roman!</h1>
--- src/app/write/layout.tsx
+++ src/app/write/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Konkani Speech Dataset Write Page",
+    description: "Here the users will write Konkani sentences in Roman and Devanagari scripts and its translation in English.",
+};
+
 export default function WriteLayout() {
     return <h1>Listen!</h1>
 }
